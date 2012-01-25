9fea99236b05d7eab59b5d0d9a28414c50290888
Jo Bovy <bovy@ias.edu>
2012-01-25 03:34:45 +0000

    apogee: start on test module
    git-svn-id: file:///Users/bovy/Repos/apogee_svn@4 f4176b12-edc3-40a8-afa5-87b8ecf07db3

---
+++ apogee/test/testVlos.py
@@ -0,0 +1,41 @@
+#Module for running tests of the los velocities
+import numpy
+import fitsio
+import apogee
+datafile= apogee.tools.apallPath(nodups=nodups)
+data= fitsio.read(datafile,1)
+def repeatedMeasurements(mjdrange=None,postshutdown=False):
+    """
+    NAME:
+       repeatedMeasurements
+    PURPOSE:
+       build the distribution of repeated measurements of vlos
+    INPUT:
+       mjdrange= if not None, use this MJD range, or use
+       postshutdown= (default: False) if True, only use post-shutdown data
+    OUTPUT:
+       array with uncertainty normalized residuals 
+       (i.e, (vlos-<vlos>)/sigma_vlos for repeated measurements
+    HISTORY:
+       2012-01-24 - Written - Bovy (2012)
+    """
+    #Read data
+    data= fitsio.read(datafile,1)
+    indx= numpy.array(['STAR' in data['OBJTYPE'][ii] for ii in range(len(data))],dtype='bool')
+    data= data[indx]
+    if postshutdown:
+        data= data[(data['MJD5'] > 55788)]
+    if not mjdrange is None:
+        data= data[(data['MJD5'] > mjdrange[0])*(data['MJD5'] < mjdrange[1])]
+    out= []
+    primarydata= data[data.specprimary]
+    ndata= len(primarydata)
+    for ii in range(ndata):
+        thesedata= data[(data.uniqid == primarydata[ii].specid)]
+        indx= (thesedata.vraderr != 0.)
+        if numpy.sum(indx) < 2:
+            continue
+        thesedata= thesedata[indx]
+        meanvrad= numpy.mean(thesedata.vrad)
+        out.extend((thesedata.vrad-meanvrad)/thesedata.vraderr)
+    return numpy.array(out)
