cc5c46f69e357ccddf9bfa068c07923cf4bc3d2e
Jo Bovy <bovy@ias.edu>
2012-05-30 21:38:04 +0000

    apogee: edit path for aspcap
    git-svn-id: file:///Users/bovy/Repos/apogee_svn@9 f4176b12-edc3-40a8-afa5-87b8ecf07db3

--- apogee/tools/path.py
+++ apogee/tools/path.py
@@ -1,8 +1,12 @@
 import os, os.path
 _APOGEE_DATA= os.getenv('APOGEE_DATA')
 _APOGEE_REDUX= os.getenv('APOGEE_REDUX')
+_APOGEE_ASPCAP_REDUX= os.getenv('APOGEE_ASPCAP_REDUX')
 if _APOGEE_REDUX is None:
     _APOGEE_REDUX= 'v0.91'
+if _APOGEE_ASPCAP_REDUX is None:
+    _APOGEE_ASPCAP_REDUX= 'v0.91'
+_ASPCAP= True
 def apallPath():
     """
     NAME:
@@ -17,7 +21,13 @@ def apallPath():
        APOGEE_REDUX with the current reduction version (e.g., v0.91)
     HISTORY:
        2012-01-02 - Written - Bovy (IAS)
+       2012-05-30 - Edited for ASPCAP - Bovy (IAS)
     """
-    return os.path.join(_APOGEE_DATA,
-                        'apall-'+_APOGEE_REDUX+'.fits')
+    if _ASPCAP:
+        return os.path.join(_APOGEE_DATA,
+                            'apall-1d-'+_APOGEE_REDUX
+                            +'-aspcap-'+_APOGEE_ASPCAP_REDUX+'.fits')
+    else:
+        return os.path.join(_APOGEE_DATA,
+                            'apall-'+_APOGEE_REDUX+'.fits')
     
