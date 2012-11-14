762b93c4b2e3cbc1b0d44ab7e6671664687c1116
Jo Bovy <bovy@ias.edu>
2012-11-14 02:46:45 +0000

    apogee: new path
    git-svn-id: file:///Users/bovy/Repos/apogee_svn@12 f4176b12-edc3-40a8-afa5-87b8ecf07db3

--- apogee/tools/path.py
+++ apogee/tools/path.py
@@ -5,15 +5,17 @@ _APOGEE_ASPCAP_REDUX= os.getenv('APOGEE_ASPCAP_REDUX')
 if _APOGEE_REDUX is None:
     _APOGEE_REDUX= 'v0.91'
 if _APOGEE_ASPCAP_REDUX is None:
-    _APOGEE_ASPCAP_REDUX= 'v0.3'
+    _APOGEE_ASPCAP_REDUX= 'v0.4'
 _ASPCAP= True
-def apallPath():
+_CODEV= '2'
+def apallPath(visit=False):
     """
     NAME:
        apallPath
     PURPOSE:
-       returns the path of the aprvall file
+       returns the path of the relevant file
     INPUT:
+       visit= if True, return the allVisit file, rather than the allStar file
     OUTPUT:
        path string
     REQUIREMENTS:
@@ -23,11 +25,18 @@ def apallPath():
        2012-01-02 - Written - Bovy (IAS)
        2012-05-30 - Edited for ASPCAP - Bovy (IAS)
     """
-    if _ASPCAP:
-        return os.path.join(_APOGEE_DATA,
-                            'apall-1d-'+_APOGEE_REDUX
-                            +'-aspcap-'+_APOGEE_ASPCAP_REDUX+'.fits')
-    else:
-        return os.path.join(_APOGEE_DATA,
-                            'apall-'+_APOGEE_REDUX+'.fits')
-    
+    if _CODEV == '1':
+        if _ASPCAP:
+            return os.path.join(_APOGEE_DATA,
+                                'apall-1d-'+_APOGEE_REDUX
+                                +'-aspcap-'+_APOGEE_ASPCAP_REDUX+'.fits')
+        else:
+            return os.path.join(_APOGEE_DATA,
+                                'apall-'+_APOGEE_REDUX+'.fits')
+    elif _CODEV == '2':
+        if visit:
+            pass
+        else:
+            return os.path.join(_APOGEE_DATA,
+                                'allStar-'+_APOGEE_ASPCAP_REDUX+'.fits')
+
