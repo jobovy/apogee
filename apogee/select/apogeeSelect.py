82add02f2af2f1901a68de7936e165e54370f0ba
Jo Bovy <bovy@ias.edu>
2013-11-19 12:25:34 -0500

    fix minnspec requirement in a few placs

--- apogee/select/apogeeSelect.py
+++ apogee/select/apogeeSelect.py
@@ -554,13 +554,13 @@ class apogeeSelect:
             location= self._locations
         elif isinstance(location,str) and location.lower() == 'short':
             cohort= 'short'
-            location= self._locations[(numpy.nanmax(self._short_completion,axis=1) >= self._frac4complete)*(self._nspec_short > 0.)]
+            location= self._locations[(numpy.nanmax(self._short_completion,axis=1) >= self._frac4complete)*(self._nspec_short >= self._minnspec)]
         elif isinstance(location,str) and location.lower() == 'medium':
             cohort= 'medium'
-            location= self._locations[(numpy.nanmax(self._medium_completion,axis=1) >= self._frac4complete)*(self._nspec_medium > 0.)]
+            location= self._locations[(numpy.nanmax(self._medium_completion,axis=1) >= self._frac4complete)*(self._nspec_medium >= self._minnspec)]
         elif isinstance(location,str) and location.lower() == 'long':
             cohort= 'long'
-            location= self._locations[(numpy.nanmax(self._long_completion,axis=1) >= self._frac4complete)*(self._nspec_long > 0.)]
+            location= self._locations[(numpy.nanmax(self._long_completion,axis=1) >= self._frac4complete)*(self._nspec_long >= self._minnspec)]
         if isinstance(location,(numpy.int16,int)): #Scalar input
             location= [location]
         #Gather data from all requested locations and cohorts
@@ -801,13 +801,13 @@ class apogeeSelect:
             location= self._locations
         elif isinstance(location,str) and location.lower() == 'short':
             cohort= 'short'
-            location= self._locations[(numpy.nanmax(self._short_completion,axis=1) >= self._frac4complete)*(self._nspec_short > 0.)]
+            location= self._locations[(numpy.nanmax(self._short_completion,axis=1) >= self._frac4complete)*(self._nspec_short >= self._minnspec)]
         elif isinstance(location,str) and location.lower() == 'medium':
             cohort= 'medium'
-            location= self._locations[(numpy.nanmax(self._medium_completion,axis=1) >= self._frac4complete)*(self._nspec_medium > 0.)]
+            location= self._locations[(numpy.nanmax(self._medium_completion,axis=1) >= self._frac4complete)*(self._nspec_medium >= self._minnspec)]
         elif isinstance(location,str) and location.lower() == 'long':
             cohort= 'long'
-            location= self._locations[(numpy.nanmax(self._long_completion,axis=1) >= self._frac4complete)*(self._nspec_long > 0.)]
+            location= self._locations[(numpy.nanmax(self._long_completion,axis=1) >= self._frac4complete)*(self._nspec_long >= self._minnspec)]
         if isinstance(location,(numpy.int16,int)): #Scalar input
             location= [location]
             scalarOut= True
