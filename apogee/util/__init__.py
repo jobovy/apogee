1088c960ab450e70d6295d04d916a63a6f87dee7
Jo Bovy <bovy@ias.edu>
2014-02-28 11:33:15 -0500

    add local feh dist

---
+++ apogee/util/__init__.py
@@ -0,0 +1,6 @@
+import numpy
+def localfehdist(feh):
+    #From 2 Gaussian XD fit to Casagrande et al. (2011)
+    fehdist= 0.8/0.15*numpy.exp(-0.5*(feh-0.016)**2./0.15**2.)\
+        +0.2/0.22*numpy.exp(-0.5*(feh+0.15)**2./0.22**2.)
+    return fehdist
