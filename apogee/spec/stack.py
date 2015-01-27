7d629ccd5fc3e4f25ed60b1f0e05508de5f13a39
Jo Bovy <bovy@ias.edu>
2015-01-26 22:18:25 -0800

    start on simple stacking module

---
+++ apogee/spec/stack.py
@@ -0,0 +1,28 @@
+###############################################################################
+# apogee.spec.stack: stack APOGEE spectra in various ways
+###############################################################################
+import numpy
+def median(spec,mask=None):
+    """
+    NAME:
+       median
+    PURPOSE:
+       median stack a set of spectra
+    INPUT:
+       spec - array of spectra (nspec,nwave)
+       mask= (None) if set, use this mask (1/True for inclusion)
+    OUTPUT:
+       median spectrum
+    HISTORY:
+       2015-01-26 - Written - Bovy (IAS@KITP)
+    """
+    mask= mask.astype('bool')
+    if mask is None:
+        mask= True-numpy.isnan(spec)
+    else:
+        mask*= True-numpy.isnan(spec)
+    
+    out= numpy.zeros(spec.shape[1])+numpy.nan
+    for ii in range(spec.shape[1]):
+        out[ii]= numpy.median(spec[mask[:,ii],ii])
+    return out
