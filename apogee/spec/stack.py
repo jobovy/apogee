495890abd2407dd603f9d1007554e56c79bde6d0
Jo Bovy <bovy@ias.edu>
2015-02-05 16:15:47 -0800

    inverse-variance stacking

--- apogee/spec/stack.py
+++ apogee/spec/stack.py
@@ -2,6 +2,7 @@
 # apogee.spec.stack: stack APOGEE spectra in various ways
 ###############################################################################
 import numpy
+_BIGERR= 10.**7.
 def median(spec,mask=None):
     """
     NAME:
@@ -25,3 +26,30 @@ def median(spec,mask=None):
     for ii in range(spec.shape[1]):
         out[ii]= numpy.median(spec[mask[:,ii],ii])
     return out
+
+def invvar(spec,specerr=None,return_error=False):
+    """
+    NAME:
+       invvar
+    PURPOSE:
+       Inverse-variance stack a set of spectra
+    INPUT:
+       spec - array of spectra (nspec,nwave)
+       specerr - (None) if set, use these errors
+       return_error= (False) if True, also return the error on the stack
+    OUTPUT:
+       (stacked spectrum,error)
+    HISTORY:
+       2015-02-05 - Written - Bovy (IAS@KITP)
+    """
+    if specerr is None:
+        specerr= numpy.ones_like(spec)
+    spec[numpy.isnan(spec)]= 2.
+    specerr[numpy.isnan(spec)]= _BIGERR
+    err2= 1./numpy.sum(1./specerr**2.,axis=0)
+    out= numpy.sum(spec/specerr**2.,axis=0)*err2
+    out[out == 2.]= numpy.nan
+    if return_error:
+        return (out,numpy.sqrt(err2))
+    else:
+        return out
