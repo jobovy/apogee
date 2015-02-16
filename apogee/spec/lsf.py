cf87935c15dd157991a6e044a85ecff3a7150e83
Jo Bovy <bovy@ias.edu>
2015-02-15 22:18:40 -0800

    function to unpack the LSF parameters

---
+++ apogee/spec/lsf.py
@@ -0,0 +1,53 @@
+###############################################################################
+# apogee.spec.lsf: Utilities to work with APOGEE LSFs
+###############################################################################
+import numpy
+def unpack_lsf_params(lsfarr):
+    """
+    NAME:
+       unpack_lsf_params
+    PURPOSE:
+       Unpack the LSF parameter array into its constituents
+    INPUT:
+       lsfarr - the parameter array
+    OUTPUT:
+       dictionary with unpacked parameters and parameter values
+    HISTORY:
+       2015-02-15 - Written based on Nidever's code in apogeereduce - Bovy (IAS@KITP)
+    """
+    out= {}
+    # binsize: The width of a pixel in X-units
+    out['binsize']= lsfarr[0]
+    # X0: An additive x-offset; used for GH parameters that vary globally
+    out['Xoffset']= lsfarr[1]
+    # Horder: The highest Hermite order
+    out['Horder']= int(lsfarr[2])
+    # Porder: Polynomial order array for global variation of each LSF parameter
+    out['Porder']= lsfarr[3:out['Horder']+4]
+    out['Porder']= out['Porder'].astype('int')
+    nGHcoefs= numpy.sum(out['Porder']+1)
+    # GHcoefs: Polynomial coefficients for sigma and the Horder Hermite parameters
+    maxPorder= numpy.amax(out['Porder'])
+    GHcoefs= numpy.zeros((out['Horder']+1,maxPorder+1))
+    GHpar= lsfarr[out['Horder']+4:out['Horder']+4+nGHcoefs] #all coeffs
+    CoeffStart= numpy.hstack((0,numpy.cumsum(out['Porder']+1)))
+    for ii in range(out['Horder']+1):
+        GHcoefs[ii,:out['Porder'][ii]+1]= GHpar[CoeffStart[ii]:CoeffStart[ii]+out['Porder'][ii]+1]
+    out['GHcoefs']= GHcoefs
+    # Wproftype: Wing profile type
+    wingarr= lsfarr[3+out['Horder']+1+nGHcoefs:]
+    out['Wproftype']= int(wingarr[0])
+    # nWpar: Number of wing parameters
+    out['nWpar']= int(wingarr[1])
+    # WPorder: Polynomial order for the global variation of each wing parameter
+    out['WPorder']= wingarr[2:2+out['nWpar']]
+    out['WPorder']= out['WPorder'].astype('int')
+    # Wcoefs: Polynomial coefficients for the wings parameters
+    maxWPorder= numpy.amax(out['WPorder'])
+    Wcoefs= numpy.zeros((out['nWpar'],maxWPorder+1))
+    Wpar= wingarr[out['nWpar']+2:]
+    WingcoeffStart= numpy.hstack((0,numpy.cumsum(out['WPorder']+1)))
+    for ii in range(out['nWpar']):
+        Wcoefs[ii,:out['WPorder'][ii]+1]= Wpar[WingcoeffStart[ii]:WingcoeffStart[ii]+out['WPorder'][ii]+1]
+    out['Wcoefs']= Wcoefs
+    return out
