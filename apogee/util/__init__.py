d3994a0e5e8c2af4b02345c571e9963f8ca95aca
Jo Bovy <bovy@ias.edu>
2015-02-24 13:59:15 -0800

    add ERASESTR

--- apogee/util/__init__.py
+++ apogee/util/__init__.py
@@ -1,4 +1,5 @@
 import numpy
+_ERASESTR= "                                                                                "
 def localfehdist(feh):
     #From 2 Gaussian XD fit to Casagrande et al. (2011)
     fehdist= 0.8/0.15*numpy.exp(-0.5*(feh-0.016)**2./0.15**2.)\
